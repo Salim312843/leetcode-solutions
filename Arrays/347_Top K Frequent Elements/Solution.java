5634c3e778630dd2d31496827d57a1af3fe979db
Salim312843 <saleemmaniyar740@gmail.com>
2026-03-25 11:07:52 +0530

    Improve comments in topKFrequent method
    Refactor comments for clarity in topKFrequent method.

--- Arrays/347_Top K Frequent Elements/Solution.java
+++ Arrays/347_Top K Frequent Elements/Solution.java
@@ -5,23 +5,23 @@ class Solution {
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // Step 1: Count frequency
-        for (int num : nums) {
+        // Step 1: Count frequency                 
+        for (int num : nums) {                                         
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         // Step 2: Create bucket array
         List<Integer>[] bucket = new ArrayList[nums.length + 1];
 
-        for (int key : map.keySet()) {
-            int freq = map.get(key);
+        for (int key : map.keySet()) {           // 1, 2, 3
+            int freq = map.get(key);             // 1 → 3  2 → 2  3 → 1
 
             if (bucket[freq] == null) {
-                bucket[freq] = new ArrayList<>();
-            }
+                bucket[freq] = new ArrayList<>();          // bucket[1] → [3]
+                }                                            // bucket[2] → [2]
+             bucket[freq].add(key);                                           //bucket[3] → [1]
+            } 
 
-            bucket[freq].add(key);
-        }
 
         // Step 3: Collect top k elements
         int[] result = new int[k];
