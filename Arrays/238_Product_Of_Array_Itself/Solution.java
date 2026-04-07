2bf53a0fda7c7d64d01589ea107e0fe6f7615df5
Salim312843 <saleemmaniyar740@gmail.com>
2026-04-07 10:59:29 +0530

    Refactor productExceptSelf method for clarity

--- Arrays/238_Product_Of_Array_Itself/Solution.java
+++ Arrays/238_Product_Of_Array_Itself/Solution.java
@@ -1,20 +1,20 @@
+import java.util.*;
+
 class Solution {
     public int[] productExceptSelf(int[] nums) {
 
         int n = nums.length;
         int[] answer = new int[n];
 
-        // Step 1: Store prefix product in answer[]
-        answer[0] = 1;
+        answer[0] = 1;  // no left elements
         for (int i = 1; i < n; i++) {
-            answer[i] = answer[i - 1] * nums[i - 1];
+            answer[i] = answer[i - 1] * nums[i - 1];  // prefix product
         }
 
-        // Step 2: Multiply with suffix product
-        int suffix = 1;
+        int suffix = 1;  // no right elements
         for (int i = n - 1; i >= 0; i--) {
-            answer[i] = answer[i] * suffix;
-            suffix *= nums[i];
+            answer[i] *= suffix;   // combine prefix & suffix
+            suffix *= nums[i];     // update suffix
         }
 
         return answer;
