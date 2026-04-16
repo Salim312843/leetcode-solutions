d7855bb95dd49545001ea9b72330e4696c6119ed
Salim312843 <saleemmaniyar740@gmail.com>
2026-04-16 09:22:32 +0530

    Refactor comments in longestConsecutive method
    Updated comments for clarity and added explanation for HashSet usage.

--- Arrays/128_Longest Consecutive Sequence/Solution.java
+++ Arrays/128_Longest Consecutive Sequence/Solution.java
@@ -1,16 +1,21 @@
+// APPROACH : HASHSET
+// TIME COMPLEXITY : O(n)
+// SPACE COMPLEXITY : O(n)
+
+
 import java.util.*;
 
 class Solution {
     public int longestConsecutive(int[] nums) {
 
         HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) {
+        for (int num : nums) {          // Insert all numbers into set   // Removes duplicates automatically
             set.add(num);
         }
 
         int longest = 0;
 
-        for (int num : set) {
+        for (int num : set) {        // Iterate through unique elements
 
             if (!set.contains(num - 1)) {   // start of sequence
 
